5e8757a56e0fce8a907a51e0c49846ef73dbf2f7
Zeb Williams <zebulonw2@gmail.com>
2022-09-01 13:53:26 -0700

    convert post views to class-based

--- polling/views.py
+++ polling/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def list_view(request):
-    context = {"polls": Poll.objects.all()}
-    return render(request, "polling/list.html", context)
+class PollListView(ListView):
+    model = Poll
+    template_name = "polling/list.html"
 
 
-def detail_view(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = "polling/detail.html"
 
-    if request.method == "POST":
-        if request.POST.get("vote") == "Yes":
-            poll.score += 1
-        else:
-            poll.score -= 1
-        poll.save()
-
-    context = {"poll": poll}
-    return render(request, "polling/detail.html", context)
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+        if request.method == "POST":
+            if request.POST.get("vote") == "Yes":
+                poll.score += 1
+            else:
+                poll.score -= 1
+            poll.save()
+            context = {"object": poll}
+            return render(request, "polling/detail.html", context)
