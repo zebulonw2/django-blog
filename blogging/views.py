7e2d5707e740e6ab7a5e3de67fc71b7f20755c40
Zeb Williams <zebulonw2@gmail.com>
2022-09-06 23:12:22 -0700

    csrf cookies

--- blogging/views.py
+++ blogging/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .forms import PostForm
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 
 
 class PostList(ListView):
@@ -18,9 +19,11 @@ class PostDetail(DetailView):
         return Post.objects.exclude(published_date__exact=None)
 
 
+@csrf_protect
 def add_post(request):
     context = {}
     form = PostForm(request.POST or None, request.FILES or None)
+    # form = PostForm(request.POST)
     if form.is_valid():
         form.save()
     context["form"] = form
